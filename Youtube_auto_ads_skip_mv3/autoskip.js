61600e1043648e469aa5ca95a09ed92f954afaea
herogate92 <45154898+herogate92@users.noreply.github.com>
2023-09-11 11:40:16 +0900

    Add files via upload

---
+++ Youtube_auto_ads_skip_mv3/autoskip.js
@@ -0,0 +1,22 @@
+
+window.onload = function(){
+    setInterval(function () {
+        if (document.getElementsByClassName("ytp-ad-skip-button")[0] != undefined) {
+            console.log('Find Video Ads & Run Auto Skip');
+            document.getElementsByClassName("html5-main-video")[0].currentTime = 999;
+            document.getElementsByClassName("html5-main-video")[0].play();
+            document.getElementsByClassName("ytp-ad-skip-button")[0].click();
+        }
+
+        if (document.getElementsByClassName("ytp-ad-overlay-close-button")[0] != undefined) {
+            console.log('Find Popup Ads & Close Popup Ads');
+            document.getElementsByClassName("ytp-ad-overlay-close-button")[0].click();
+        }
+
+        if (document.getElementsByClassName("ytp-ad-player-overlay-skip-or-preview")[0] != undefined) {
+            console.log('Find Short Video Ads & Skip');
+            document.getElementsByClassName("html5-main-video")[0].currentTime = 999;
+        }
+
+    }, 250);
+}   
